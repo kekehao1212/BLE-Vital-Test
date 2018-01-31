3d7c68cc66336358fee4a2f310bb243b3cdb7925
kekehao1212 <kekehao1212@outlook.com>
2018-01-31 18:30:19 +0800

    first edition

--- app.js
+++ app.js
@@ -1,55 +1,118 @@
 //app.js
 import loading from './state/loading'
+var amapFile = require('./utils/amap-wx.js')
 App({
-  onLaunch () {
+  onLaunch() {
     this.longitude = undefined
     this.latitude = undefined
   },
-  onShow () {
-    ((!this.student || !this.myTeacher) && !this.signUping) ? this.studentLogin() : null
+  onShow() {
+    ;(!this.student || !this.myTeacher) && !this.signUping
+      ? this.getCertificate()
+      : null
   },
-  studentLogin() {
-    loading.showLoading('正在加載个人信息',true)
-    wx.login({
-      success: (res) => {
-        const code = res.code;
-        wx.request({
-          url: `${this.url}/wx/student/login`,
-          header: { WX_CODE: code },
-          method: 'POST',
-          data: {},
-          success: (res) => {
-            console.log(res)
-            loading.hideLoading()
-            this.sessionId = res.header.WX_SESSION_ID;
-            console.log(this.sessionId)
-            if (!res.data.student) {
-              this.signUping = true
-              wx.reLaunch({
-                url: '../signUp/signUp',
-              })
-              return
-            }
-            this.student = res.data.student
-            this.getTeacher()
+  wxLogin() {
+    return new Promise((resolve, reject) => {
+      wx.login({
+        success: res => {
+          resolve(res.code)
+        },
+        fail: res => {
+          reject('get code error')
+        }
+      })
+    })
+  },
+  loginFail() {
+    loading.hideLoading()
+    wx.showModal({
+      title: '登录失败',
+      content: '请稍后重试',
+      showCancel: false
+    })
+  },
+  codeLogin() {
+    this.wxLogin()
+      .then(code => {
+        this.studentLogin({
+          WX_CODE: code
+        })
+          .then(sessionId => {
+            wx.setStorage({
+              key: 'sessionId',
+              data: sessionId
+            })
+          })
+          .catch(res => {
+            this.loginFail()
+          })
+      })
+      .catch(res => {
+        this.loginFail()
+      })
+  },
+  getCertificate() {
+    loading.showLoading(' 正在加载个人信息...', true)
+    var sessionId
+    var sessionId = wx.getStorageSync('sessionId')
+    console.log('storage', sessionId)
+    if (!sessionId) {
+      this.codeLogin()
+    } else {
+      this.studentLogin({
+        WX_SESSION_ID: sessionId
+      })
+        .then(res => {})
+        .catch(res => {
+          if (res === 'needCode') {
+            this.codeLogin()
           }
-        })  
-      }
+        })
+    }
+  },
+  studentLogin(header) {
+    return new Promise((resolve, reject) => {
+      wx.request({
+        url: `${this.url}/wx/student/login`,
+        header: header,
+        method: 'POST',
+        data: {},
+        success: res => {
+          console.log(res)
+          if (res.header.WX_SESSION_ID) {
+            resolve(res.header.WX_SESSION_ID)
+          } else {
+            reject('needCode')
+            return
+          }
+          loading.hideLoading()
+          this.sessionId = res.header.WX_SESSION_ID
+          console.log(this.sessionId)
+          if (!res.data.student) {
+            this.signUping = true
+            wx.reLaunch({
+              url: '../signUp/signUp'
+            })
+            return
+          }
+          this.student = res.data.student
+          this.getTeacher()
+        }
+      })
     })
   },
 
   getTeacher() {
-    loading.showLoading('正在加载教师信息',true)
+    loading.showLoading('正在加载教师信息', true)
     wx.request({
       url: `${this.url}/wx/student/supervisor/my`,
       method: 'GET',
       header: { WX_SESSION_ID: this.sessionId },
-      success: (res) => {
-        console.log('121212',res)
+      success: res => {
         loading.hideLoading()
         if (res.statusCode === 404) {
           wx.reLaunch({
-            url: '../chooseHappening/chooseHappening',
+            url: '../chooseHappening/chooseHappening'
           })
         } else {
           this.myTeacher = res.data
@@ -58,26 +121,27 @@ App({
     })
   },
 
-  authorFail () {
+  authorFail() {
     wx.showModal({
       title: '警告',
-      content: '您之前拒绝授权，将无法正常使用该功能，若要使用该功能，请重新授权小程序',
-      success: (res) => {
+      content:
+        '您之前拒绝授权，将无法正常使用该功能，若要使用该功能，请重新授权小程序',
+      success: res => {
         if (res.confirm) {
           wx.openSetting({
-            success: (res) => {
+            success: res => {
               if (res.authSetting['scope.userLocation']) {
                 this.Location()
-              } else{
+              } else {
                 wx.reLaunch({
-                  url: '../home/home',
+                  url: '../home/home'
                 })
               }
             }
           })
         } else {
           wx.reLaunch({
-            url: '../home/home',
+            url: '../home/home'
           })
         }
       }
@@ -86,22 +150,23 @@ App({
 
   getLocation() {
     wx.getSetting({
-      success: (res) => {
+      success: res => {
         if (res.authSetting['scope.userLocation'] === undefined) {
           wx.showModal({
             title: '提示',
-            content: '为了使用该功能，请接下来授权小程序获取您的位置信息，拒绝授权将无法正常使用',
+            content:
+              '为了使用该功能，请接下来授权小程序获取您的位置信息，拒绝授权将无法正常使用',
             showCancel: false,
-            success: (res) => {
+            success: res => {
               if (res.confirm) {
                 wx.authorize({
                   scope: 'scope.userLocation',
-                  success: (res) => {
+                  success: res => {
                     this.Location()
                   },
-                  fail: (res) => {
+                  fail: res => {
                     this.authorFail()
-                  } 
+                  }
                 })
               }
             }
@@ -115,20 +180,36 @@ App({
     })
   },
 
-  Location () {
+  Location() {
     loading.showLoading('正在定位...', true)
     if (!this.latitude || !this.longitude) {
-      wx.getLocation({
-        success: (res) => {
+      var myMap = new amapFile.AMapWX({
+        key: 'accc413a9ef333ef7a5d79e659274a03'
+      })
+      var timeout = setTimeout(() => {
+        wx.showModal({
+          title: '请打开wifi加快定位获取',
+          showCancel: false
+        })
+      }, 6000)
+      myMap.getRegeo({
+        success: res => {
+          clearTimeout(timeout)
+          console.log(res)
+          this.latitude = res[0].latitude
+          this.longitude = res[0].longitude
           loading.hideLoading()
-          this.longitude = res.longitude
-          this.latitude = res.latitude
-        },fail: (res) => {
+        },
+        fail: res => {
+          wx.getLocation({
+            success: res => {
+              this.latitude = res.latitude
+              this.longitude = res.longitude
+            }
+          })
           loading.hideLoading()
-          this.Location()
         }
       })
-      return
     } else {
       loading.hideLoading()
     }
